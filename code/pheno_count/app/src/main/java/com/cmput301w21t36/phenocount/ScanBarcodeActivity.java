e0a203717f84ce0d962dc1b7edb7e327bfce7b34
anthony <anthony4@ualberta.ca>
2021-03-08 21:30:04 -0700

    Added Scan Barcode Dependencies

---
+++ code/pheno_count/app/src/main/java/com/cmput301w21t36/phenocount/ScanBarcodeActivity.java
@@ -0,0 +1,27 @@
+package com.cmput301w21t36.phenocount;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.zxing.integration.android.IntentIntegrator;
+
+public class ScanBarcodeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scan_barcode);
+
+        Button button = findViewById(R.id.button2);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new IntentIntegrator(ScanBarcodeActivity.this).initiateScan();
+            }
+        });
+    }
+}
