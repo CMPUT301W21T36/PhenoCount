2513b9c61808e21c07f6ffb2751fbcbc97cbd5c0
anthony <anthony4@ualberta.ca>
2021-03-08 20:15:48 -0700

    Added Scan QR Dependencies

---
+++ code/pheno_count/app/src/main/java/com/cmput301w21t36/phenocount/ScanQRActivity.java
@@ -0,0 +1,14 @@
+package com.cmput301w21t36.phenocount;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ScanQRActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scan_q_r);
+    }
+}
