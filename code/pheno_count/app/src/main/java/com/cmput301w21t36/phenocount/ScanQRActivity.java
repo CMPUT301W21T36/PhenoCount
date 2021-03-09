2c6ac0b11099dd6224e2692e4b51f6624712f033
anthony <anthony4@ualberta.ca>
2021-03-08 20:41:22 -0700

    Updated Scan QR Activity

--- code/pheno_count/app/src/main/java/com/cmput301w21t36/phenocount/ScanQRActivity.java
+++ code/pheno_count/app/src/main/java/com/cmput301w21t36/phenocount/ScanQRActivity.java
@@ -2,7 +2,14 @@ package com.cmput301w21t36.phenocount;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
 
 public class ScanQRActivity extends AppCompatActivity {
 
@@ -10,5 +17,14 @@ public class ScanQRActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scan_q_r);
+
+        Button button = findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new IntentIntegrator(ScanQRActivity.this).initiateScan();
+            }
+        });
     }
 }
