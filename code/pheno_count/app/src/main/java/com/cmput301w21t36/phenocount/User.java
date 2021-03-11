20358660ef5b9b18091d07d7a09f48dde3245033
KaleburGit <caleb.lonson@gmail.ca>
2021-03-11 14:35:20 -0700

    Pull from main

--- code/pheno_count/app/src/main/java/com/cmput301w21t36/phenocount/User.java
+++ code/pheno_count/app/src/main/java/com/cmput301w21t36/phenocount/User.java
@@ -2,10 +2,18 @@ package com.cmput301w21t36.phenocount;
 
 public class User {
 
-    private String ID;
+    private String UID = "111"; // This is a garbage UID that will be updated when connected to Firestore
+    private String displayName;
     private String contact;
 
+
+
+    public void setDisplayName(String displayName) {
+        this.displayName = displayName;
+    }
+
     public void setContact(String contact) {
+
         this.contact = contact;
     }
 }
