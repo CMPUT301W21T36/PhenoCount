5aa3753c9c16c533e84046a0ddb3570e853e54d5
marzookh <farook@ualberta.ca>
2021-03-14 16:41:59 +0400

    commented reply parameters changed to .document()

--- code/pheno_count/app/src/main/java/com/cmput301w21t36/phenocount/MainActivity.java
+++ code/pheno_count/app/src/main/java/com/cmput301w21t36/phenocount/MainActivity.java
@@ -61,8 +61,6 @@ public class MainActivity extends AppCompatActivity {
         // Will get instance of the database
         db = FirebaseFirestore.getInstance();
 
-        DocumentReference userReference;
-
 
        /* Experiment exp = new Experiment("Coin Flip", "We flip a coin in this experiment","North America","Binomial", 10, true);
         expDataList.add(exp);
@@ -87,7 +85,7 @@ public class MainActivity extends AppCompatActivity {
          */
 
         if (firstStart) {
-            userReference = db.collection("User").document();
+            final DocumentReference userReference = db.collection("User").document();
             // Auto-ID created for the document
             String GrabbedID = userReference.getId();
 
@@ -116,26 +114,22 @@ public class MainActivity extends AppCompatActivity {
 
         UUID = sharedPrefs.getString(AutoID, "");
 
-        System.out.println(UUID);
-
 
         /**
          * Will retrieve the Username for the user and set the variable username
          * to the returned String
          */
-        /*
-        This section of code is causing some errors for people and not too sure as to why
-        Will leave it commented out for now
-        userReference = db.collection("User").document(UUID);
-        userReference.get()
+        DocumentReference userRef = db.collection("User").document();
+            userRef.get()
                 .addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
                     @Override
                     public void onSuccess(DocumentSnapshot documentSnapshot) {
                         username = documentSnapshot.getString("Username");
+                        System.out.println(username);
                     }
                 });
 
-         */
+
         profileButton = findViewById(R.id.profileButton);
 
  //////////////////// h
@@ -177,7 +171,6 @@ public class MainActivity extends AppCompatActivity {
 
     public void openProfile(){
         Intent intent = new Intent(this, ProfileActivity.class);
-        intent.putExtra("UID", UUID);
         startActivity(intent);
     }
 
