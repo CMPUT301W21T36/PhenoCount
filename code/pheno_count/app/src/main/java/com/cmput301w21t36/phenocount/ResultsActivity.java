e60f2ddb78dc7cbe136b19878fe9c56faf606fc2
anthony <anthony4@ualberta.ca>
2021-03-19 13:04:56 -0600

    Added Generate Qr Code

--- code/pheno_count/app/src/main/java/com/cmput301w21t36/phenocount/ResultsActivity.java
+++ code/pheno_count/app/src/main/java/com/cmput301w21t36/phenocount/ResultsActivity.java
@@ -1,15 +1,21 @@
 package com.cmput301w21t36.phenocount;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.ListView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.ArrayList;
+
+import androidmads.library.qrgenearator.QRGContents;
+import androidmads.library.qrgenearator.QRGEncoder;
 
 /**
  * This model class passes a trials list to TrialAdapter to display results of the trials conducted
@@ -20,6 +26,7 @@ public class ResultsActivity extends AppCompatActivity {
     ArrayAdapter<Trial> trialAdapter;
     ArrayList<Trial> trialList;
     Experiment exp;//defining the Experiment object
+    ImageView qr;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +37,7 @@ public class ResultsActivity extends AppCompatActivity {
         //initializing attributes
         trials =findViewById(R.id.trial_list);
         trialList = new ArrayList<>();
+        qr = findViewById(R.id.qrView);
 
         //getting intent
         exp = (Experiment) getIntent().getSerializableExtra("experiment");//defining the Experiment object
@@ -42,9 +50,34 @@ public class ResultsActivity extends AppCompatActivity {
         trials.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Intent intent = new Intent (ResultsActivity.this, GenerateQrActivity.class);
-                intent.putExtra("data", 1); //change this
-                startActivity(intent);
+                qr.setVisibility(View.VISIBLE);
+                trials.setVisibility(View.GONE);
+                // Create QR Encoder with value to be encoded
+                String type = exp.getExpType();
+                String trial = "null";
+                switch (type){
+                    case "Binomial":
+                        trial = String.valueOf(((Binomial) trialList.get(position)).getResult());
+                        break;
+                    case "Count":
+                        trial = String.valueOf(((Count) trialList.get(position)).getCount());
+                        break;
+                    case "Measurement":
+                        trial = String.valueOf(((Measurement) trialList.get(position)).getMeasurement());
+                        break;
+                    case "NonNegativeCount":
+                        trial = String.valueOf(((NonNegativeCount) trialList.get(position)).getValue());
+                        break;
+                }
+                QRGEncoder qrgEncoder = new QRGEncoder(trial, null, QRGContents.Type.TEXT, 500);
+                try {
+                    // Getting QR as Bitmap
+                    Bitmap bitmap = qrgEncoder.getBitmap();
+                    // Set QR to ImageView
+                    qr.setImageBitmap(bitmap);
+                } catch (Exception e) {
+                    Log.v("Exception", e.toString());
+                }
             }
         });
     }
