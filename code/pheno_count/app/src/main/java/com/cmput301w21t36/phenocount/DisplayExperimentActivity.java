2e0f0167e46577a90869cb3eb99b5db7f0377876
Anisha <sethumad@ualberta.ca>
2021-04-01 17:28:33 +0530

    Unpublish and end an exp functionalities

--- code/pheno_count/app/src/main/java/com/cmput301w21t36/phenocount/DisplayExperimentActivity.java
+++ code/pheno_count/app/src/main/java/com/cmput301w21t36/phenocount/DisplayExperimentActivity.java
@@ -17,6 +17,8 @@ import android.widget.TextView;
 
 import com.google.firebase.firestore.FirebaseFirestore;
 
+import java.util.ArrayList;
+
 /**
  * This activity deals with displaying the contents of an experiment
  * To access this activity: Open the app -> click on the listView
@@ -30,19 +32,26 @@ public class DisplayExperimentActivity extends AppCompatActivity {
     private String username;
     private String UUID;
     private ExpManager expManager;
+    Menu expMenu;
+    TextView expStatus;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_experiment_display);
         exp = (Experiment) getIntent().getSerializableExtra("experiment");//defining the Experiment object
+        db = FirebaseFirestore.getInstance();
+        SharedPreferences sharedPrefs = getSharedPreferences("sharedPrefs", MODE_PRIVATE);
+        sharedPrefs = getSharedPreferences("sharedPrefs", MODE_PRIVATE);
+        username = sharedPrefs.getString("Username", "");
+        UUID = sharedPrefs.getString("ID", "");
 
         TextView expName = findViewById(R.id.nameTextView);
         TextView expDesc = findViewById(R.id.descTextView);
         TextView expOwner = findViewById(R.id.ownerTextView);
         TextView expRegion = findViewById(R.id.regionTextView);
         TextView expMinTrial = findViewById(R.id.minTrialView);
-        TextView expStatus = findViewById(R.id.statusTextView);
+        expStatus = findViewById(R.id.statusTextView);
         TextView expType = findViewById(R.id.expTypeText);
         TextView expReqLoc = findViewById(R.id.reqLocText);
 
@@ -89,49 +98,87 @@ public class DisplayExperimentActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.example_menu, menu);
+        expMenu = menu;
+        menuOpt();
         return true;
     }
 
+    void menuOpt() {
+        expMenu.findItem(R.id.ownerAction).setVisible(true);
+        expMenu.findItem(R.id.unpublishButton).setEnabled(true);
+        expMenu.findItem(R.id.endButton).setEnabled(true);
+        expMenu.findItem(R.id.addTrialButon).setEnabled(true);
+
+        if(!(UUID.equals(exp.getOwner().getUID()))){
+            expMenu.findItem(R.id.ownerAction).setVisible(false);
+        }
+        if (exp.getExpStatus()==3){
+            expMenu.findItem(R.id.unpublishButton).setEnabled(false);
+        }
+        if (exp.getExpStatus()==2){
+            expMenu.findItem(R.id.endButton).setEnabled(false);
+            expMenu.findItem(R.id.addTrialButon).setEnabled(false);
+        }
+    }
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-            if(item.getItemId() == R.id.item1) {
-                if (exp.getExpType().equals("Binomial")) {
-                    Intent bintent = new Intent(DisplayExperimentActivity.this, BinomialActivity.class);
-                    bintent.putExtra("experiment", exp);
-                    int LAUNCH_SECOND_ACTIVITY = 1;
-                    startActivityForResult(bintent, LAUNCH_SECOND_ACTIVITY);
-                }
-                if (exp.getExpType().equals("Count")) {
-                    Intent cintent = new Intent(DisplayExperimentActivity.this, CountActivity.class);
-                    cintent.putExtra("experiment", exp);
-                    int LAUNCH_SECOND_ACTIVITY = 1;
-                    startActivityForResult(cintent, LAUNCH_SECOND_ACTIVITY);
-                }
-                if (exp.getExpType().equals("Measurement")) {
-                    Intent mintent = new Intent(DisplayExperimentActivity.this, MeasurementActivity.class);
-                    mintent.putExtra("experiment", exp);
-                    int LAUNCH_SECOND_ACTIVITY = 1;
-                    startActivityForResult(mintent, LAUNCH_SECOND_ACTIVITY);
-                }
-                if (exp.getExpType().equals("NonNegativeCount")) {
-                    Intent nintent = new Intent(DisplayExperimentActivity.this, NonNegativeCountActivity.class);
-                    nintent.putExtra("experiment", exp);
-                    int LAUNCH_SECOND_ACTIVITY = 1;
-                    startActivityForResult(nintent, LAUNCH_SECOND_ACTIVITY);
-                }
-            }
-            else if(item.getItemId() == R.id.item3) {
-                Intent dintent = new Intent(DisplayExperimentActivity.this, DiscussionActivity.class);
-                dintent.putExtra("experiment", exp);
+        if (item.getItemId() == R.id.addTrialButon) {
+            if (exp.getExpType().equals("Binomial")) {
+                Intent bintent = new Intent(DisplayExperimentActivity.this, BinomialActivity.class);
+                bintent.putExtra("experiment", exp);
                 int LAUNCH_SECOND_ACTIVITY = 1;
-                startActivity(dintent);
+                startActivityForResult(bintent, LAUNCH_SECOND_ACTIVITY);
             }
-            else if (item.getItemId() == R.id.item4){
-                Intent tintent = new Intent(DisplayExperimentActivity.this, ResultsActivity.class);
-                tintent.putExtra("experiment", exp);
-                startActivity(tintent);
+            if (exp.getExpType().equals("Count")) {
+                Intent cintent = new Intent(DisplayExperimentActivity.this, CountActivity.class);
+                cintent.putExtra("experiment", exp);
+                int LAUNCH_SECOND_ACTIVITY = 1;
+                startActivityForResult(cintent, LAUNCH_SECOND_ACTIVITY);
             }
-        return super.onOptionsItemSelected(item);
+            if (exp.getExpType().equals("Measurement")) {
+                Intent mintent = new Intent(DisplayExperimentActivity.this, MeasurementActivity.class);
+                mintent.putExtra("experiment", exp);
+                int LAUNCH_SECOND_ACTIVITY = 1;
+                startActivityForResult(mintent, LAUNCH_SECOND_ACTIVITY);
+            }
+            if (exp.getExpType().equals("NonNegativeCount")) {
+                Intent nintent = new Intent(DisplayExperimentActivity.this, NonNegativeCountActivity.class);
+                nintent.putExtra("experiment", exp);
+                int LAUNCH_SECOND_ACTIVITY = 1;
+                startActivityForResult(nintent, LAUNCH_SECOND_ACTIVITY);
+            }
+        } else if (item.getItemId() == R.id.item3) {
+            Intent dintent = new Intent(DisplayExperimentActivity.this, DiscussionActivity.class);
+            dintent.putExtra("experiment", exp);
+            int LAUNCH_SECOND_ACTIVITY = 1;
+            startActivity(dintent);
+        } else if (item.getItemId() == R.id.item4) {
+            Intent tintent = new Intent(DisplayExperimentActivity.this, ResultsActivity.class);
+            tintent.putExtra("experiment", exp);
+            startActivity(tintent);
+        /*} else if (item.getItemId() == R.id.subscribeButton) {
+            Profile currentUserProfile = new Profile(username);
+            User currentUser = new User(UUID, currentUserProfile);
+            //ArrayList<Experiment> subList = new ArrayList<>();
+            //subList = currentUser.getExpSubscribed();
+            ArrayList<Experiment> subList = currentUser.getExpSubscribed();
+            subList.add(exp);
+            */
+        } else if (item.getItemId() == R.id.unpublishButton) {
+                // this query updates the unpublish status
+            db.collection("Experiment").document(exp.getExpID())
+                        .update("status", "3");
+            exp.setExpStatus(3);
+            menuOpt();
+            expStatus.setText("Unpublished");
+        } else if (item.getItemId() == R.id.endButton){
+            db.collection("Experiment").document(exp.getExpID())
+                    .update("status", "2");
+            exp.setExpStatus(2);
+            menuOpt();
+            expStatus.setText("Ended");
+        }
+            return super.onOptionsItemSelected(item);
     }
 
     @Override
@@ -157,4 +204,6 @@ public class DisplayExperimentActivity extends AppCompatActivity {
                 System.out.println("No Data");
             }
         }
+
+
     }
