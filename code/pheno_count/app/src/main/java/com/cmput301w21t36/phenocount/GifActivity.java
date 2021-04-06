f892bbde19321c6dda6b544404481fad05d088bd
Charffy <baohui@ualberta.ca>
2021-04-06 14:40:38 -0600

    adjust the splash

---
+++ code/pheno_count/app/src/main/java/com/cmput301w21t36/phenocount/GifActivity.java
@@ -0,0 +1,15 @@
+package com.cmput301w21t36.phenocount;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+    public class GifActivity extends AppCompatActivity {
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setTheme(R.style.Theme_PhenoCount);
+            setContentView(R.layout.activity_main);
+
+        }
+    }
