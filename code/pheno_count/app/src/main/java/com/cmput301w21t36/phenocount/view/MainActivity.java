2d006ca14d7c8f82e6476e3de5fd3f213df0b237
Anisha <sethumad@ualberta.ca>
2021-04-08 04:29:31 +0530

    color legend

--- code/pheno_count/app/src/main/java/com/cmput301w21t36/phenocount/view/MainActivity.java
+++ code/pheno_count/app/src/main/java/com/cmput301w21t36/phenocount/view/MainActivity.java
@@ -1,14 +1,17 @@
+//References:
+// codedmin, 2017-10-26, https://www.codingdemos.com/android-options-menu-icon/#:~:text=Click%20res%20%E2%86%92%20New%20%E2%86%92,grouped%20under%20a%20single%20Menu%20.
 package com.cmput301w21t36.phenocount;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
-import android.app.Notification;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
@@ -17,7 +20,6 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
-import android.widget.Toolbar;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -52,6 +54,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     NavigationView navigationView;
     androidx.appcompat.widget.Toolbar toolbar;
 
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -60,7 +63,6 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
         TextView toolBarTitle = (TextView)findViewById(R.id.toolbar_title);
         toolBarTitle.setText("My Experiments");
-
         navigationSettings();
 
         experiments = findViewById(R.id.expList);
@@ -139,6 +141,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         });
         expAdapter.notifyDataSetChanged();
     }
+
 
     /**
      * This method is called when the addButton is clicked and Switches MainActivity to
