4a10bc92ffd3a851b62095c513440e6e6a68a375
Ananya <ananyarao2807@gmail.com>
2021-04-07 22:09:51 +0400

    subscribed exp tool+nav bar

--- code/pheno_count/app/src/main/java/com/cmput301w21t36/phenocount/ShowSubscribedListActivity.java
+++ code/pheno_count/app/src/main/java/com/cmput301w21t36/phenocount/ShowSubscribedListActivity.java
@@ -1,19 +1,26 @@
 package com.cmput301w21t36.phenocount;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.util.ArrayList;
 
-public class ShowSubscribedListActivity extends AppCompatActivity {
+public class ShowSubscribedListActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
     FirebaseFirestore db;
     ListView subExperiments;
     ArrayList<Experiment> expDataList;
@@ -21,6 +28,9 @@ public class ShowSubscribedListActivity extends AppCompatActivity {
     ExpManager manager = new ExpManager();
     String UUID;
     DatabaseManager dbManager = new DatabaseManager();
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
+    androidx.appcompat.widget.Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,7 +38,7 @@ public class ShowSubscribedListActivity extends AppCompatActivity {
         setTheme(R.style.Theme_PhenoCount);
         setContentView(R.layout.activity_show_subscribed_list);
         //getSupportActionBar().setTitle("My Subscribed Experiments");
-
+        navigationSettings();
         db = dbManager.getDb();
         UUID = getIntent().getExtras().getString("owner");
         subExperiments = findViewById(R.id.subList);
@@ -51,6 +61,62 @@ public class ShowSubscribedListActivity extends AppCompatActivity {
             }
         });
         expAdapter.notifyDataSetChanged();
+    }
+
+    public void navigationSettings(){
+        drawerLayout=findViewById(R.id.drawer_layout);
+        navigationView=findViewById(R.id.nav_view);
+        toolbar = findViewById(R.id.toolbar);
+        navigationView.bringToFront();
+        setSupportActionBar(toolbar);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this,drawerLayout,toolbar,R.string.navigation_drawer_open,R.string.navigation_drawer_close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+        navigationView.setNavigationItemSelectedListener(this);
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        if(drawerLayout.isDrawerOpen(GravityCompat.START)){
+            drawerLayout.closeDrawer(GravityCompat.START);
+        }
+        else{
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        SharedPreferences sharedPrefs = getSharedPreferences("sharedPrefs", MODE_PRIVATE);
+        String uUID = sharedPrefs.getString("ID", "");
+        Intent intent = new Intent();
+        switch (item.getItemId()){
+            case R.id.nav_my_exp:
+                intent = new Intent(ShowSubscribedListActivity.this,MainActivity.class);
+                break;
+            case R.id.nav_search:
+                intent = new Intent(ShowSubscribedListActivity.this,SearchingActivity.class);
+                break;
+            case R.id.nav_user:
+                intent = new Intent(ShowSubscribedListActivity.this,ProfileActivity.class);
+                intent.putExtra("UUID",uUID);
+                break;
+            case R.id.nav_add:
+                intent = new Intent(ShowSubscribedListActivity.this,PublishExperimentActivity.class);
+                intent.putExtra("AutoId",uUID);
+                intent.putExtra("mode",0);
+                break;
+            case R.id.nav_sub_exp:
+                intent = new Intent(ShowSubscribedListActivity.this,ShowSubscribedListActivity.class);
+                intent.putExtra("owner",uUID);
+                break;
+
+        }
+
+        startActivity(intent);
+        return true;
     }
 
 
