3fb857072e21b67c7f7f846d06f7484bd58abbf0
anthony <anthony4@ualberta.ca>
2021-04-07 14:09:36 -0600

    Changed how count works

--- code/pheno_count/app/src/main/java/com/cmput301w21t36/phenocount/view/CountActivity.java
+++ code/pheno_count/app/src/main/java/com/cmput301w21t36/phenocount/view/CountActivity.java
@@ -27,8 +27,8 @@ import java.text.DecimalFormat;
  * This class represents Count trials and is part of the GUI
  */
 public class CountActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
-    Count trial;
-    Experiment newexp;//defining the Experiment object
+    com.cmput301w21t36.phenocount.Count trial;
+    com.cmput301w21t36.phenocount.Experiment newexp;//defining the Experiment object
     Boolean location=false;
     DecimalFormat numberFormat;
     TextView coordinates;
@@ -51,12 +51,11 @@ public class CountActivity extends AppCompatActivity implements NavigationView.O
         navigationSettings();
 
         // receiving intent object
-        newexp = (Experiment) getIntent().getSerializableExtra("experiment");//defining the Experiment object
+        newexp = (com.cmput301w21t36.phenocount.Experiment) getIntent().getSerializableExtra("experiment");//defining the Experiment object
 
         // get the intent object from the Qr activity
         if (newexp == null) {
-            newexp = (Experiment) getIntent().getSerializableExtra("QrExperiment");
-            qrCount = (Integer) getIntent().getSerializableExtra("count");
+            newexp = (com.cmput301w21t36.phenocount.Experiment) getIntent().getSerializableExtra("QrExperiment");
         }
 
         //setting user to owner of trial
@@ -66,7 +65,7 @@ public class CountActivity extends AppCompatActivity implements NavigationView.O
         String UUID = sharedPrefs.getString("ID", "");
         Profile profile = new Profile(username,number);
         User user = new User(UUID,profile);
-        trial = new Count(user);
+        trial = new com.cmput301w21t36.phenocount.Count(user);
         //newexp.getDates().add(trial.getDate());
         //setting type of trial
         trial.setType("Count");
@@ -125,7 +124,7 @@ public class CountActivity extends AppCompatActivity implements NavigationView.O
         });
 
         if (qrCount != -1) {
-            trial.setCount(qrCount);
+            trial.setCount(1);
             recordcountbtn.performClick();
         }
 
@@ -154,7 +153,7 @@ public class CountActivity extends AppCompatActivity implements NavigationView.O
             public void onClick(View v) {
 
                 //passing trial object to get location updated
-                Intent intent = new Intent (CountActivity.this,MapsActivity.class);
+                Intent intent = new Intent (CountActivity.this, com.cmput301w21t36.phenocount.MapsActivity.class);
                 intent.putExtra("trial_obj",trial);
 
                 int LAUNCH_SECOND_ACTIVITY = 1;
@@ -174,11 +173,11 @@ public class CountActivity extends AppCompatActivity implements NavigationView.O
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
         if (item.getItemId() == R.id.myList) {
-            Intent intent = new Intent(CountActivity.this, MainActivity.class);
+            Intent intent = new Intent(CountActivity.this, com.cmput301w21t36.phenocount.MainActivity.class);
             startActivity(intent);
         }
         if (item.getItemId() == R.id.search) {
-            Intent intent = new Intent(CountActivity.this, SearchingActivity.class);
+            Intent intent = new Intent(CountActivity.this, com.cmput301w21t36.phenocount.SearchingActivity.class);
             startActivity(intent);
         }
         return super.onOptionsItemSelected(item);
@@ -193,7 +192,7 @@ public class CountActivity extends AppCompatActivity implements NavigationView.O
             if(resultCode == Activity.RESULT_OK){
                 location = true;
                 //catching the trial object back
-                trial = (Count) data.getSerializableExtra("trial_obj");
+                trial = (com.cmput301w21t36.phenocount.Count) data.getSerializableExtra("trial_obj");
 
                 if(trial.getLatitude() == 200 && trial.getLongitude() == 200) //location has not been added as these values can never be achieved.
                     coordinates.setText("Location : NOT ADDED");
@@ -237,22 +236,22 @@ public class CountActivity extends AppCompatActivity implements NavigationView.O
         Intent intent = new Intent();
         switch (item.getItemId()){
             case R.id.nav_my_exp:
-                intent = new Intent(CountActivity.this,MainActivity.class);
+                intent = new Intent(CountActivity.this, com.cmput301w21t36.phenocount.MainActivity.class);
                 break;
             case R.id.nav_search:
-                intent = new Intent(CountActivity.this,SearchingActivity.class);
+                intent = new Intent(CountActivity.this, com.cmput301w21t36.phenocount.SearchingActivity.class);
                 break;
             case R.id.nav_user:
-                intent = new Intent(CountActivity.this,ProfileActivity.class);
+                intent = new Intent(CountActivity.this, com.cmput301w21t36.phenocount.ProfileActivity.class);
                 intent.putExtra("UUID",UUID);
                 break;
             case R.id.nav_add:
-                intent = new Intent(CountActivity.this,PublishExperimentActivity.class);
+                intent = new Intent(CountActivity.this, com.cmput301w21t36.phenocount.PublishExperimentActivity.class);
                 intent.putExtra("AutoId",UUID);
                 intent.putExtra("mode",0);
                 break;
             case R.id.nav_sub_exp:
-                intent = new Intent(CountActivity.this,ShowSubscribedListActivity.class);
+                intent = new Intent(CountActivity.this, com.cmput301w21t36.phenocount.ShowSubscribedListActivity.class);
                 intent.putExtra("owner",UUID);
                 break;
 
